f8f97d3249972d85cfb26c5881f9eeef1ccbaaae
utkina-ekaterina <utkinaek@my.yorku.ca>
2025-01-12 00:09:38 -0500

    Added UI and buttons, added animation

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Ekaterina Utkina on 2025-01-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
